4adde0c2f1d2d94661984f7af3630dad41f5dfdd
OoO256 <qjrmsktso2@gmail.com>
2019-05-05 02:59:40 +0900

    im tired

--- linking_loader.h
+++ linking_loader.h
@@ -10,4 +10,5 @@ int linking_loader(int num_files, char* file1, char* file2, char* file3);
 
 extern unsigned int progaddr;
 extern unsigned int execute_addr;
+extern int total_length;
 #endif //PROJECT_LINKING_LOADER_H
