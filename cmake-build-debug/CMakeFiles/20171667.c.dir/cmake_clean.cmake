47accc5f0ec166758d959e1c8ecca02639e76548
undergraduate <cse20171667@cspro3.sogang.ac.kr>
2019-05-06 05:58:50 -0700

    remove files which are not for submmition

--- cmake-build-debug/CMakeFiles/20171667.c.dir/cmake_clean.cmake
+++
@@ -1,5 +0,0 @@
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/20171667.c.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
--- cmake-build-debug/CMakeFiles/20171667.c.dir/depend.make
+++
@@ -1,2 +0,0 @@
-# Empty dependencies file for 20171667.c.
-# This may be replaced when dependencies are built.
